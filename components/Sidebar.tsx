6febfd4700001b9b55d66f587753867a9d506072
SRINIVAS KALYAN <gvvkalyan@gmail.com>
2024-10-06 14:34:45 +0530

    Navbar for tablet+ devices

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { sidebarLinks } from "@/constants";
-import { cn } from "@/lib/utils";
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
@@ -12,14 +11,15 @@ const Sidebar = () => {
     <section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px] ">
       <div className="flex flex-1 flex-col gap-6">
         {sidebarLinks.map((link) => {
-          const isActive =
-            pathname == link.route;
+          const isActive = pathname == link.route;
 
           return (
             <Link
               href={link.route}
               key={link.label}
-              className={`flex gap-4 items-center p-4 rounded-lg justify-start ${isActive ? 'bg-blue-1' : ''} `}
+              className={`flex gap-4 items-center p-4 rounded-lg justify-start ${
+                isActive ? "bg-blue-1" : ""
+              } `}
             >
               <Image
                 src={link.imgUrl}
