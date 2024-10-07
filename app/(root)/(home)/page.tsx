6b73b2ef52921f4dc514a4cd4cb7cdd902a96ef7
SRINIVAS KALYAN <gvvkalyan@gmail.com>
2024-10-07 12:50:14 +0530

    Hero-card on Home page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,27 @@
 import React from "react";
 
 const Home = () => {
+  const now = new Date();
+  const time = now.toLocaleTimeString("en-IN", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  const date = new Intl.DateTimeFormat("en-IN", { dateStyle: "full" }).format(
+    now
+  );
   return (
     <section className="flex size-full flex-col gap-10 text-white">
-      <h1 className="text-3xl font-bold">Home</h1>
+      <div className="h-[300px] rounded-[20px] w-full bg-cover bg-hero">
+        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 md:max-lg:px-5 md:max-lg:py-8 lg:px-10 lg:py-8">
+          <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base text-normal">
+            Upcoming Meeting at {time} PM
+          </h2>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
+            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
