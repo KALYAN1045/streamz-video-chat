d1e62dec73ef4ad728172e03a3949dea66935399
SRINIVAS KALYAN <gvvkalyan@gmail.com>
2024-10-08 22:01:36 +0530

    Setup stream client and connected it

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,10 @@
+import { StreamCallProvider } from "@stream-io/video-react-sdk";
 import React, { ReactNode } from "react";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <main>
-      {children}
+      <StreamCallProvider>{children}</StreamCallProvider>
     </main>
   );
 };
--- components/MeetingModal.tsx
+++ components/MeetingModal.tsx
@@ -3,10 +3,6 @@ import Image from "next/image";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog";
 import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
